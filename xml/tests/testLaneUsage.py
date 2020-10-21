978fb572a75584468096ff234d3729d696bda5ba
Karina J. Burns <kevin.burns.eit@gmail.com>
2020-10-21 07:33:20 -0500

    Beginnings of Lane Usage XSD tests

--- xml/tests/testLaneUsage.py
+++ xml/tests/testLaneUsage.py
@@ -1,6 +1,8 @@
 import unittest
 from uuid import uuid4 as uuid
 from lxml import etree
+from typing import Callable
+from abc import abstractmethod
 
 
 class LaneUsageConstants:
@@ -38,7 +40,13 @@ def create_and_add_clean_lane_policy(attach_to: etree.ElementBase) -> etree.Elem
     return node
 
 
-class CleanLaneUsageDocument:
+class IValidatesDocument:
+    @abstractmethod
+    def validate(self) -> bool:
+        return False
+
+
+class CleanLaneUsageDocument(IValidatesDocument):
     def __init__(self):
         ns_map = {"xsi": 'http://www.w3.org/2001/XMLSchema-instance'}
 
@@ -61,7 +69,7 @@ class CleanLaneUsageDocument:
         fp.close()
 
     def validate(self) -> bool:
-        xs_doc = etree.parse('../vehicle-types.xsd')
+        xs_doc = etree.parse('../lane-usage.xsd')
         xsd = etree.XMLSchema(xs_doc)
         document = etree.ElementTree(self._document_root)
         return xsd.validate(document)
@@ -70,10 +78,66 @@ class CleanLaneUsageDocument:
         return self._policies_node
 
 
-class TestsForLanePolicies(unittest.TestCase):
+class TestCaseWithUuidChecker(unittest.TestCase):
+    @property
+    @abstractmethod
+    def doc(self) -> IValidatesDocument:
+        return None
+
+    def check_uuid_values(self, setter: Callable[[str], None]) -> None:
+        test_tuples = [
+            ('', False),
+            ('62d987f6-becc-430e-a8fa-c940f8fa0a86', True),
+            ('62d987g6-be3c-430e-a8fa-c940f8fa0a86', False)
+        ]
+        for (value, expected_result) in test_tuples:
+            setter(value)
+            self.assertEqual(self.doc.validate(), expected_result)
+
+
+class TestsForLanePolicies(TestCaseWithUuidChecker):
+    @property
+    def doc(self) -> IValidatesDocument:
+        return self._doc
+
     def setUp(self) -> None:
         self._doc = CleanLaneUsageDocument()
         self.target_node = self._doc.get_policies_node()
+
+    def test_that_collection_is_required(self): pass
+
+    def test_collection_member_counts(self): pass
+
+    def test_that_name_is_optional(self): pass
+
+    def test_name_values(self): pass
+
+    def test_that_uuid_is_required(self): pass
+
+    def test_uuid_values(self):
+        node = create_and_add_clean_lane_policy(self.target_node)
+
+        def setter(text: str) -> None:
+            node.attrib[LanePolicyConstants.POLICY_UUID_ATTR] = text
+
+        self.check_uuid_values(setter)
+
+    def test_that_start_from_is_required(self): pass
+
+    def test_start_from_values(self): pass
+
+    def test_except_count(self): pass
+
+    def test_that_except_uuid_is_required(self): pass
+
+    def test_except_uuid_values(self):
+        node = create_and_add_clean_lane_policy(self.target_node)
+        exc = create_lane_policy_except(node, str(uuid()))
+
+        def setter(text: str) -> None:
+            exc.attrib[LanePolicyConstants.EXCEPT_GROUP_ATTR] = text
+
+        self.check_uuid_values(setter)
 
 
 if __name__ == '__main__':
