4767088b121de9a6f8e2cbce04a5615247cddcd0
Karina J. Burns <kevin.burns.eit@gmail.com>
2020-10-27 21:35:27 -0500

    XSD Tests for Lane Policies complete.
    Still need to:
    . Do default file
    . Do tests for default file

--- xml/tests/testLaneUsage.py
+++ xml/tests/testLaneUsage.py
@@ -74,6 +74,9 @@ class CleanLaneUsageDocument(IValidatesDocument):
         document = etree.ElementTree(self._document_root)
         return xsd.validate(document)
 
+    def get_root_node(self):
+        return self._document_root
+
     def get_policies_node(self):
         return self._policies_node
 
@@ -94,6 +97,11 @@ class TestCaseWithUuidChecker(unittest.TestCase):
             setter(value)
             self.assertEqual(self.doc.validate(), expected_result)
 
+    def check_effect_of_removing_node(self, remove_from: etree.ElementBase, attribute_name: str,
+                                      expected_result: bool) -> None:
+        remove_from.attrib.pop(attribute_name)
+        self.assertEqual(self.doc.validate(), expected_result)
+
 
 class TestsForLanePolicies(TestCaseWithUuidChecker):
     @property
@@ -104,15 +112,44 @@ class TestsForLanePolicies(TestCaseWithUuidChecker):
         self._doc = CleanLaneUsageDocument()
         self.target_node = self._doc.get_policies_node()
 
-    def test_that_collection_is_required(self): pass
+    def test_that_collection_is_required(self):
+        nodes_to_keep = list(filter(lambda node: node.tag != LanePolicyConstants.COLLECTION_TAG,
+                                    self._doc.get_root_node()))
+        self._doc.get_root_node()[:] = nodes_to_keep
+        self.assertFalse(self._doc.validate())
 
-    def test_collection_member_counts(self): pass
+    def test_collection_member_counts(self):
+        test_tuples = [
+            (0, False),
+            (1, True),
+            (4, True),
+            (100, True),
+        ]
+        for (count, expected_result) in test_tuples:
+            self.target_node[:] = []
+            for _ in range(count):
+                create_and_add_clean_lane_policy(self.target_node)
+            self.assertEqual(self._doc.validate(), expected_result)
 
-    def test_that_name_is_optional(self): pass
+    def test_that_name_is_optional(self):
+        node = create_and_add_clean_lane_policy(self.target_node)
+        node.attrib.pop(LanePolicyConstants.POLICY_NAME_ATTR)
+        self.assertTrue(self._doc.validate())
 
-    def test_name_values(self): pass
+    def test_name_values(self):
+        test_tuples = [
+            ('', True),
+            ('name', True),
+            ('2291', True),
+        ]
+        node = create_and_add_clean_lane_policy(self.target_node)
+        for (value, expected_result) in test_tuples:
+            node.attrib[LanePolicyConstants.POLICY_NAME_ATTR] = value
+            self.assertEqual(self._doc.validate(), expected_result)
 
-    def test_that_uuid_is_required(self): pass
+    def test_that_uuid_is_required(self):
+        node = create_and_add_clean_lane_policy(self.target_node)
+        self.check_effect_of_removing_node(node, LanePolicyConstants.POLICY_UUID_ATTR, False)
 
     def test_uuid_values(self):
         node = create_and_add_clean_lane_policy(self.target_node)
@@ -122,13 +159,40 @@ class TestsForLanePolicies(TestCaseWithUuidChecker):
 
         self.check_uuid_values(setter)
 
-    def test_that_start_from_is_required(self): pass
+    def test_that_start_from_is_required(self):
+        node = create_and_add_clean_lane_policy(self.target_node)
+        self.check_effect_of_removing_node(node, LanePolicyConstants.POLICY_START_FROM_ATTR, False)
 
-    def test_start_from_values(self): pass
+    def test_start_from_values(self):
+        test_tuples = [
+            ('', False),
+            (LanePolicyConstants.POLICY_START_FROM_VALUE_ALL, True),
+            (LanePolicyConstants.POLICY_START_FROM_VALUE_NONE, True),
+            ('something else', False),
+        ]
+        node = create_and_add_clean_lane_policy(self.target_node)
+        for (value, expected_result) in test_tuples:
+            node.attrib[LanePolicyConstants.POLICY_START_FROM_ATTR] = value
+            self.assertEqual(self._doc.validate(), expected_result)
 
-    def test_except_count(self): pass
+    def test_except_count(self):
+        test_tuples = [
+            (0, True),
+            (1, True),
+            (4, True),
+            (1000, True),
+        ]
+        node = create_and_add_clean_lane_policy(self.target_node)
+        for (count, expected_value) in test_tuples:
+            node[:] = []
+            for _ in range(count):
+                create_lane_policy_except(node, str(uuid()))
+            self.assertEqual(self._doc.validate(), expected_value)
 
-    def test_that_except_uuid_is_required(self): pass
+    def test_that_except_uuid_is_required(self):
+        node = create_and_add_clean_lane_policy(self.target_node)
+        ex = create_lane_policy_except(node, str(uuid()))
+        self.check_effect_of_removing_node(ex, LanePolicyConstants.EXCEPT_GROUP_ATTR, False)
 
     def test_except_uuid_values(self):
         node = create_and_add_clean_lane_policy(self.target_node)
