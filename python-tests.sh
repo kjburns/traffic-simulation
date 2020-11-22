f03b60838cb088013b8f81629a719494129af399
Karina J. Burns <kevin.burns.eit@gmail.com>
2020-11-22 00:40:40 -0600

    Updated python tests script to include simulator tests.

--- python-tests.sh
+++ python-tests.sh
@@ -5,17 +5,40 @@ echo -e "./python-tests.sh"
 echo -e "    Run tests and show summary after each test file"
 echo -e "./python-tests.sh -v"
 echo -e "    Show result of each test individually (for troubleshooting)"
-cd default-files
+
+# Tests in default xml files
+(
+echo -e "\033[91m"
+cd default-files || exit
 for f in *.py
 do
   echo -e $f
   python3 $f $1
 done
-cd ..
-cd xml/tests
+)
+
+# Tests for xsd files
+(
+echo -e "\033[92m"
+cd xml/tests || exit
 for f in *.py
 do
   echo -e $f
   python3 $f $1
 done
-cd ../..
+)
+
+# tests for simulator
+(
+echo -e "\033[93m"
+cd src/py || exit
+IFS=
+while read -r file
+do
+  echo -e $file
+  rootname=${file:2:-3}
+  python3 -m ${rootname//\//.} $1
+done < <(find . -type f -name "test_*.py" -print)
+)
+
+echo -e "\033[0m"
