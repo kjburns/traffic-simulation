eb14f17cf03c91687b80688d7df80d7a8b61e85f
Karina J. Burns <kevin.burns.eit@gmail.com>
2020-12-24 09:33:41 -0600

    Test committing with token

--- src/py/i18n_l10n/en_us.py
+++ src/py/i18n_l10n/en_us.py
@@ -2,7 +2,7 @@ class LocalizationDictionaryEnUS:
     @staticmethod
     def get_dictionary():
         return {
-            'D0001': 'Unit name may not be blank or whitespace only.',
+            'D0001': 'Unit name may not be blank or whitespace only. ',
             'D0002': 'Unit factor may not be zero.',
             'W0001': 'Vehicle has trailer but no articulation point was provided.'
                      'Articulation point assumed to be at back tip of vehicle.',
